62a00dd483e08f7d8f4d5c44dea4d6c668f7af34
Alexander Kozachuk <kozachuk.a.p@gmail.com>
2016-03-14 21:52:42 +0300

    ITM-13: comments was deleted from main-class

--- src/com/kozachuk/ita/Main.java
+++ src/com/kozachuk/ita/Main.java
@@ -14,57 +14,6 @@ public class Main {
     public static void main(String[] args) throws IOException{
         Configuration conf = new Configuration(args);
         conf.parse();
-        Session session = HibernatePersistance.getSessionFactory().openSession();
-
-
-        /*ApplicationState state = new ListeningState();
-        state = state.next(StateType.ONE);
-        state.setSession(session);
-        Respond respond = state.handle();*/
-        /*System.out.println(respond.getMessage());
-        System.out.println(respond.getContent());
-
-        state = state.next(StateType.TWO);
-        state.setSession(session);
-        respond = state.handle();
-        System.out.println(respond.getMessage());
-        System.out.println(respond.getContent());*/
-
-
-        //HibernatePersistance.shutdown();
-        /*Session session = HibernatePersistance.getSessionFactory().openSession();
-        Respond respond = new Respond();
-        StringBuilder bstring = new StringBuilder();
-
-        Command command = new GetCategoriesCommand(session);
-        List<Category> data = command.run();
-
-        for(int i=0; i < data.size(); i++)
-            bstring.append(i + " - " + data.get(i).getName() + "; ");
-
-        respond.setContent(bstring.toString());
-
-        HibernatePersistance.shutdown();*/
-
-        //String msisdn = "88888889";
-        /*Session session = HibernatePersistance.getSessionFactory().openSession();
-
-        List<Category> categoryList = session.createCriteria(Category.class).
-                                              addOrder(Order.asc("name")).list();
-        for(Category category: categoryList){
-            System.out.println(category.getId() + " : " + category.getName());
-        }
-
-
-
-
-
-        //UserRepository repoUSer = new UserRepository(session);
-
-        //ApplicationState state = new MainState();
-
-        HibernatePersistance.shutdown();*/
-        //ApplicationState state = new MainState();
 
         Application app = Application.getApplication(conf);
         app.handle();

18e6f06be08756df98fb9e4e94184e3ea87ca1c4
Alexander Kozachuk <kozachuk.a.p@gmail.com>
2016-03-14 21:53:31 +0300

    ITM-14: useless imports was removed

--- src/com/kozachuk/ita/Main.java
+++ src/com/kozachuk/ita/Main.java
@@ -1,9 +1,7 @@
 package com.kozachuk.ita;
 
 import com.kozachuk.ita.Configuration.Configuration;
-import com.kozachuk.ita.Persistance.HibernatePersistance;
 import com.kozachuk.ita.Application.Application;
-import org.hibernate.Session;
 import java.io.IOException;
 /**
  * Created by alexanderkozachuk on 11.03.16.
